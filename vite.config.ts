8e742518eebbce3265bf55acde6ed739566e73af
ngattusohw <ngattusohw@gmail.com>
2025-01-07 00:45:34 -0500

    Add vite-plugin-static-copy to dependencies, updating versions and resolving dependencies for fs-extra, graceful-fs, jsonfile, and universalify.

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,20 @@
-import path from "path"
+import path from 'path';
 import { defineConfig } from 'vite';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        { src: '_redirects', dest: '' }, // Copy `_redirects` to the root of `dist`
+      ],
+    }),
+  ],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
   },
   optimizeDeps: {
